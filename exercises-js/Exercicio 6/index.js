26b1f94cb6a088c04b3193368ab4fbecc2195a39
Edilson Pacheco <44613606+diipacheco@users.noreply.github.com>
2018-11-07 15:09:00 -0200

    Add files via upload

---
+++ exercises-js/Exercicio 6/index.js
@@ -0,0 +1,36 @@
+
+// Funções, métodos e parâmetros \\
+
+// ========================== \\
+      
+ function add(x1, x2) {
+    return x1 + x2;
+}
+
+ let result = add(1, 2);
+
+    console.log(result);
+// ========================== \\
+function mult(x1, x2) {
+
+    return x1 * x2;
+}
+
+let result2 = mult(2, 2)
+   
+   console.log(result2);
+
+// ========================== \\
+
+function calc(x1, x2, oparator){
+
+   return eval(`${x1} ${oparator} ${x2}`);
+} 
+
+let result3 = calc(10, 10, "/");
+ 
+     console.log(result3);
+
+
+
+
